5df07586aad456132786b02a2711dc895263979b
Cody Tessler <codytessler@gmail.com>
2025-12-08 22:25:04 -0500

    Added bonus

---
+++ python-bonus/bonus1.py
@@ -0,0 +1,6 @@
+# Implement a program that prompts a user for their name, 
+# and then prints hello, so-and-so, where so-and-so is their provided name. 
+# For example:
+# $ python hello.py
+# What is your name? John
+# Hello, John
---
+++ python-bonus/bonus2.py
@@ -0,0 +1,9 @@
+# Improve the program so that if the user does not enter a name but
+# immediately presses return, the phrase "Hello, World" is printed instead. 
+# For example:
+# $ python hello.py
+# What is your name? John
+# Hello, John
+# $ python hello.py
+# What is your name?
+# Hello, world
